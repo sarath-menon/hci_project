905d0c875b061cf94a96a5ecbb96125ea471ddce
Sarath S Menon <sarathmenon.downey@gmail.com>
2024-12-01 17:43:38 +0100

    disabled API key check

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,13 +6,13 @@ import { useEffect } from "react";
 
 export function PHProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    if (
-      !process.env.NEXT_PUBLIC_POSTHOG_KEY ||
-      !process.env.NEXT_PUBLIC_POSTHOG_HOST
-    ) {
-      console.log("API keys not found");
-      return;
-    }
+    // if (
+    //   !process.env.NEXT_PUBLIC_POSTHOG_KEY ||
+    //   !process.env.NEXT_PUBLIC_POSTHOG_HOST
+    // ) {
+    //   console.log("API keys not found");
+    //   return;
+    // }
 
     posthog.init("phc_fuBfkF0LRmH68aRcjj2XDR6m3uYx3DKraUiqHA9GRLa", {
       api_host: "https://eu.i.posthog.com",
